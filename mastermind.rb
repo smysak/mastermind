e917da8a872389fd740bccf0202ab3475b17d472
smysak <98991422+smysak@users.noreply.github.com>
2026-06-19 11:28:09 +0800

    add a game menu system

--- mastermind.rb
+++ mastermind.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+require 'io/console'
 require 'colorize'
 
 # creates a code of four random numbers from 1-6
@@ -14,13 +15,39 @@ class Code
   end
 end
 
+MENU = {
+  '1' => -> { one_player_start },
+  '4' => lambda {
+    puts "\nGoodbye."
+    exit
+  }
+}.freeze
+
+def game_menu
+  puts "\n1. One-Player Game\n4. Exit"
+  $stdin.ioflush
+  menu_loop
+end
+
+def menu_loop
+  loop do
+    input = $stdin.getch
+    next unless MENU.key?(input)
+
+    MENU[input].call
+  end
+end
+
+puts "\n"
 puts 'M'.on_red + 'esta'.on_yellow + 'r'.on_red + 'inm'.on_yellow + 'd'.on_red
-sleep 0.5
+sleep 1
 3.times do
   sleep 0.5
   print '.'
   $stdout.flush
 end
 puts ''
-sleep 0.5
+sleep 1
 puts 'Mastermind'.on_red
+sleep 1
+game_menu
