ce99f80f45d789f824c3f3075492b3a08c692b28
smysak <98991422+smysak@users.noreply.github.com>
2026-06-19 09:35:13 +0800

    create class Code four random integers from 1-6 are chosen

--- mastermind.rb
+++ mastermind.rb
@@ -1,3 +1,18 @@
 # frozen_string_literal: true
 
-require colorize
+require 'colorize'
+
+# creates a code of four random numbers from 1-6
+class Code
+  attr_reader :first, :second, :third, :fourth
+
+  def initialize
+    @first = rand(1..6)
+    @second = rand(1..6)
+    @third = rand(1..6)
+    @fourth = rand(1..6)
+  end
+end
+
+test_code = Code.new
+p [test_code.first, test_code.second, test_code.third, test_code.fourth]
